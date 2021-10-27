a4e1cba3da1adbc5cad9c3e629d50ef0fe46ad69
gunnxx <wahyu.guntara@gmail.com>
2021-10-27 20:33:26 +0700

    remove resigtered_optim

--- src/algo/algo.py
+++ src/algo/algo.py
@@ -1,15 +1,4 @@
-import torch.optim as optim
-
-## For convenience only, so we can
-## specify the optimizer from JSON
-## through str datatype.
-REGISTERED_OPTIM = {
-  "adam": optim.Adam,
-  "sgd": optim.SGD
-}
-
-## For convenience only, user only
-## need to implement run().
+# For convenience only, user only need to implement run().
 class Algo:
   """
   """
