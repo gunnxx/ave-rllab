483008169afbf34adbbafdcbbee73b70d3a39a85
gunnxx <wahyu.guntara@gmail.com>
2021-11-03 14:55:51 +0700

    move to pybullet since there will be env based on mujoco engine

--- src/env/__init__.py
+++ src/env/__init__.py
@@ -1,29 +1,33 @@
 from gym.envs.registration import register
 
-register(
-  id='BrokenReacherPyBulletEnv-v0',
-  entry_point='src.env.broken_reacher:BrokenReacherBulletEnv',
-  max_episode_steps=150,
-  reward_threshold=18.0,
-  )
-
-register(
-  id='BrokenAntPyBulletEnv-v0',
-  entry_point='src.env.broken_ant:BrokenAntBulletEnv',
-  max_episode_steps=1000,
-  reward_threshold=2500.0
-  )
-
-register(
-  id='BrokenHalfCheetahPyBulletEnv-v0',
-  entry_point='src.env.broken_ant:BrokenAntBulletEnv',
-  max_episode_steps=1000,
-  reward_threshold=2500.0
-  )
+# --------------- basic env for testing ---------------
 
 register(
   id='PointEnv-v0',
   entry_point='src.env.point:PointEnv',
   max_episode_steps=20,
   reward_threshold=0.
-  )
+)
+
+# --------------- inherits `pybulletgym` using `pybullet` engine ---------------
+
+register(
+  id='BrokenReacherPyBulletEnv-v0',
+  entry_point='src.env.pybullet.broken_reacher:BrokenReacherBulletEnv',
+  max_episode_steps=150,
+  reward_threshold=18.0,
+)
+
+register(
+  id='BrokenAntPyBulletEnv-v0',
+  entry_point='src.env.pybullet.broken_ant:BrokenAntBulletEnv',
+  max_episode_steps=1000,
+  reward_threshold=2500.0
+)
+
+register(
+  id='BrokenHalfCheetahPyBulletEnv-v0',
+  entry_point='src.env.pybullet.broken_half_cheetah:BrokenHalfCheetahMujocoEnv',
+  max_episode_steps=1000,
+  reward_threshold=2500.0
+)
