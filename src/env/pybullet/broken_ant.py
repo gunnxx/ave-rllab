11e3d0da6b5357265754a68ae4095a5d3b6835a0
gunnxx <wahyu.guntara@gmail.com>
2021-11-05 19:17:33 +0700

    finalize ant env

--- src/env/pybullet/broken_ant.py
+++ src/env/pybullet/broken_ant.py
@@ -1,31 +1,60 @@
-from typing import Dict
+from typing import Dict, List
+
+import gym.spaces
+import numpy as np
+from pybulletgym.envs.mujoco.envs.locomotion.ant_env import AntMuJoCoEnv
 from torch import Tensor
-from pybulletgym.envs.roboschool.envs.locomotion.ant_env import AntBulletEnv
 
 from src.env.broken_env import BrokenEnv
 from src.utils.common import label_frame
 
-class BrokenAntBulletEnv(AntBulletEnv, BrokenEnv):
+class BrokenAntBulletEnv(AntMuJoCoEnv, BrokenEnv):
   """
   """
   def __init__(self) -> None:
-    AntBulletEnv.__init__(self)
+    AntMuJoCoEnv.__init__(self)
     BrokenEnv.__init__(self, 8, [1.]*8)
 
+    ## change the state to exclude `crfc` of the original env
+    high = np.inf * np.ones(27)
+    self.observation_space = gym.spaces.Box(-high, high)
+  
   """
+  """
+  @property
+  def goal(self):
+    ## same as None but can't use None since None can't be converted into tensor
+    ## nevertheless, reward computation ignores this value
+    return 0.
+  
+  """
+  Needed to compute reward for MPC.
+  Some parameters may not be used since this is intended to unify the API only.
   """
   @staticmethod
-  def reward_from_obs(obs: Tensor) -> Tensor:
-    pass
+  def reward(obs: Tensor, act: Tensor, next_obs: Tensor, goal: Tensor) -> Tensor:
+    return next_obs[..., 13]
 
   """
   """
   def render(self, mode: str, labels: Dict):
-    frame = AntBulletEnv.render(self, mode)
+    frame = AntMuJoCoEnv._render(self, mode)
     return label_frame(frame, **labels)
+  
+  """
+  """
+  def reset(self, actuator_damage: List[float] = None):
+    obs = AntMuJoCoEnv._reset(self)
+
+    if actuator_damage:
+      BrokenEnv.set_actuator_damage(self, actuator_damage)
+    
+    return obs[:27]
 
   """
   """
   def step(self, a):
-    return AntBulletEnv.step(self,
+    obs, rew, done, info = AntMuJoCoEnv.step(self,
       BrokenEnv.apply_damage(self, a))
+    
+    return obs[:27], rew, done, info
